e0b9e1c6273d82e0a86e3e49bfe80d393998cd71
Arthur Melo <arthurvelosogm@icloud.com>
2017-02-07 17:02:40 -0400

    Tela inicial com CollectionView e células customizadas
    Signed-off-by: Arthur Melo <arthurvelosogm@icloud.com>

---
+++ Roomer/GamesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GamesCollectionViewCell.swift
+//  Roomer
+//
+//  Created by Arthur Melo on 07/02/17.
+//  Copyright © 2017 teamsug. All rights reserved.
+//
+
+import UIKit
+
+class GamesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var gameName: UILabel!
+    @IBOutlet weak var gameImage: UIImageView!
+}
