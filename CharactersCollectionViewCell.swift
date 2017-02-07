b7d9faa0c64adb3d1565321ebe1f088ebf56ea98
Arthur Melo <arthurvelosogm@icloud.com>
2017-02-07 17:52:09 -0400

    Tela de numero de jogadores criada -Albert
    Signed-off-by: Arthur Melo <arthurvelosogm@icloud.com>

---
+++ CharactersCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharactersCollectionViewCell.swift
+//  Roomer
+//
+//  Created by Arthur Melo on 07/02/17.
+//  Copyright © 2017 teamsug. All rights reserved.
+//
+
+import UIKit
+
+class CharactersCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var characterImage: UIImageView!
+    
+}
