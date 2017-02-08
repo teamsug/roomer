570df989742028818829e3df4839e817d3310ff0
Albert Samuel Melo <asm.asm.albert@icloud.com>
2017-02-08 15:39:12 -0400

    criada cell e controller add player
    Signed-off-by: Albert Samuel Melo <asm.asm.albert@icloud.com>

---
+++ Roomer/PotatoAddPlayerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PotatoAddPlayerCollectionViewCell.swift
+//  Roomer
+//
+//  Created by Albert Samuel Melo on 08/02/17.
+//  Copyright © 2017 teamsug. All rights reserved.
+//
+
+import UIKit
+
+class PotatoAddPlayerCollectionViewCell: UICollectionViewCell {
+    
+}
